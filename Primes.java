6d3cb1b03f0a72b98557555cd43a836983565635
Or <Or@MacBook-Pro.local>
2024-12-02 00:55:14 +0200

    hw4 now

--- Primes.java
+++ Primes.java
@@ -6,38 +6,27 @@ public class Primes {
             prime[i] = true;
         }
 
-        for (int j = 4; j < prime.length; j++) {
-            if (j % 2 == 0 || j % 3 == 0) {
-                prime[j] = false;
-            }
-        }
-
-        for (int l = 6; l < prime.length; l++) {
-            if (l % 5 == 0)
-                prime[l] = false;
-        }
-
-        int p = 7;
-        int pointer = 7;
-        while (p < Math.sqrt(N)) {
-            while (pointer < prime.length) {
-                if (pointer % p == 0 && p != pointer) {
-                    prime[pointer] = false;
+        for (int j = 2; j * j <= N; j++) {
+            if (prime[j]) {
+                for (int k = j * j; k <= N; k += j) {
+                    prime[k] = false;
                 }
-                pointer++;
             }
-            p++;
         }
 
-        double counter = 0;
-        for (int m = 0; m < prime.length; m++) {
+        int counter = 0;
+        // First sentence print
+        System.out.println("Prime numbers up to " + N + ":");
+        
+        for (int m = 2; m <= N; m++) {
             if (prime[m]) {
                 System.out.println(m);
                 counter++;
             }
         }
 
-        double percentage = (counter * 100) / (N - 1);  
-        System.out.printf("There are %d primes between 2 and %d (%.0f%% are primes)\n", (int) counter, N, percentage);
+        // Percentage calculation as per the range
+        double percentage = (counter * 100.0) / (N - 1);
+        System.out.printf("There are %d primes between 2 and %d (%.0f%% are primes)\n", counter, N, percentage);
     }
 }
